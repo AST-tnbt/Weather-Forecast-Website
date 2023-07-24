310ffe64380fa3151bed3b1f3139dced4063d010
Tin Trinh <trinhtin272@gmail.com>
2023-07-24 16:13:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather-Forecast-Website
+## Introduction
+This is a simple website made in HTML, CSS, and JavaScript. Enter your city name and the current weather will be displayed.
+## Website interface
+![Website interface](./Readme-img/Website-interface.png)  
+To see the website fully, you can download this project and open index.html file or click [HERE](https://).  
+Icons file taken from this project: https://github.com/CodeExplainedRepo/Weather-App-JavaScript  
+I took favicon icon and background image on this website: https://www.freepik.com/
---
+++ main.js
@@ -0,0 +1,41 @@
+const inputLocation = document.getElementById("inputLocation");
+const currentWeather = document.getElementById("currentInformation");
+const forecastWeather = document.getElementById("forecastInformation");
+
+function getWeather(event) {
+    event.preventDefault();
+    console.log('getWeather() function called!');
+    var location = inputLocation.value.trim();
+    if (!location) {
+        alert("Please enter your city!");
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4748ad73168954570cc61e80ddedc558`)
+        .then ((response) => {
+            if (!response.ok) throw new Error("Network response is not ok")
+            return response.json();
+        })
+        .then ((data) => {
+            displayCurrentWeather(data);
+        })
+        .catch ((error) => {
+            console.error("Error: ", error);
+            currentWeather.innerHTML = `<p>An error occurred while fetching the data<br>Please check your input and try again</p>`
+        });
+}
+
+function displayCurrentWeather(data) {
+    if (data.weather && data.main && data.sys) {
+        currentWeather.innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <h3>Weather: ${data.weather[0].main}</h3>
+            <img class="weather-icon" src="./icons/${data.weather[0].icon}.png" alt="Weather icon">
+            <p>Temperature: ${Math.round(data.main.temp - 273.15)}&#176C</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind: ${Math.round(data.wind.speed * 3.6)} km/h</p>
+        `;
+    } else {
+        currentWeather.innerHTML = "<p>Weather information not available for this location.</p>";
+    } 
+}
